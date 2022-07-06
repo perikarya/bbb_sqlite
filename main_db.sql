250a1c1da5ce08f93269f97827ab595b1a7951a2
perikarya <40226185+perikarya@users.noreply.github.com>
2022-07-06 14:34:46 +0800

    Create main_db.sql

---
+++ main_db.sql
@@ -0,0 +1,14 @@
+CREATE TABLE markers (
+    gene text,
+    protein text,
+    cell_type text,
+    role text,
+    localisation text,
+    effect text
+);
+
+INSERT INTO markers VALUES ('Tjp1', 'ZO-1',	'Brain endothelial cell', 'Structural',	'Tight junction',	'Paracellular permeability');
+INSERT INTO markers VALUES ('Tjp2', 'ZO-2',	'Brain endothelial cell', 'Structural',	'Tight junction',	'Paracellular permeability');
+INSERT INTO markers VALUES ('Tjp3', 'ZO-3',	'Brain endothelial cell', 'Structural',	'Tight junction',	'Paracellular permeability');
+INSERT INTO markers VALUES ('Ocln', 'Occludin',	'Brain endothelial cell', 'Structural',	'Tight junction',	'Paracellular permeability');
+INSERT INTO markers VALUES ('Cldn5', 'Claudin-5',	'Brain endothelial cell', 'Structural',	'Tight junction',	'Paracellular permeability');
