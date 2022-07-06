f9091011242f9c4e41d8b765d48fb55e55c279c3
perikarya <40226185+perikarya@users.noreply.github.com>
2022-07-06 15:14:36 +0800

    Update main_db.sql

--- main_db.sql
+++ main_db.sql
@@ -7,8 +7,8 @@ CREATE TABLE markers (
     effect text
 );
 
-INSERT INTO markers VALUES ('Tjp1', 'ZO-1','Brain endothelial cell', 'Structural', 'Tight junction', 'Paracellular permeability');
-INSERT INTO markers VALUES ('Tjp2', 'ZO-2','Brain endothelial cell', 'Structural', 'Tight junction', 'Paracellular permeability');
-INSERT INTO markers VALUES ('Tjp3', 'ZO-3','Brain endothelial cell', 'Structural', 'Tight junction', 'Paracellular permeability');
-INSERT INTO markers VALUES ('Ocln', 'Occludin','Brain endothelial cell', 'Structural', 'Tight junction', 'Paracellular permeability');
+INSERT INTO markers VALUES ('Tjp1', 'ZO-1', 'Brain endothelial cell', 'Structural', 'Tight junction', 'Paracellular permeability');
+INSERT INTO markers VALUES ('Tjp2', 'ZO-2', 'Brain endothelial cell', 'Structural', 'Tight junction', 'Paracellular permeability');
+INSERT INTO markers VALUES ('Tjp3', 'ZO-3', 'Brain endothelial cell', 'Structural', 'Tight junction', 'Paracellular permeability');
+INSERT INTO markers VALUES ('Ocln', 'Occludin', 'Brain endothelial cell', 'Structural', 'Tight junction', 'Paracellular permeability');
 INSERT INTO markers VALUES ('Cldn5', 'Claudin-5', 'Brain endothelial cell', 'Structural', 'Tight junction','Paracellular permeability');
